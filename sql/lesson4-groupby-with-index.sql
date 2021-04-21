3f2f1894e71d855b9d010ec91a4c0d112d5c84af
koniferous22 <dekkel22@gmail.com>
2021-04-21 18:10:19 +0200

    DB indexing example

---
+++ sql/lesson4-groupby-with-index.sql
@@ -0,0 +1,59 @@
+-- Copied from lesson4-groupby
+
+CREATE TABLE roommates (
+    id          integer,
+  	name        varchar(40),
+	location    varchar(1000),
+    origin      varchar(1000)
+);
+
+INSERT INTO roommates (id, name, location, origin) VALUES (1, 'adam', 'Brno', 'Poprad');
+INSERT INTO roommates (id, name, location, origin) VALUES (2, 'vinco', 'Brno', 'Modra');
+INSERT INTO roommates (id, name, location, origin) VALUES (3, 'petra', 'Brno', 'Modra');
+INSERT INTO roommates (id, name, location, origin) VALUES (4, 'bruno', 'Brno', 'Žilina');
+INSERT INTO roommates (id, name, location, origin) VALUES (5, 'samo', 'Brno', 'Žilina');
+INSERT INTO roommates (id, name, location, origin) VALUES (6, 'matus', 'Brno', 'Prešov');
+
+UPDATE roommates SET location = 'Kysucke nove mesto' WHERE name = 'bruno';
+
+INSERT INTO roommates (id, name, location, origin) VALUES (7, 'samo', 'Brno', 'Žilina');
+
+
+ALTER TABLE roommates ADD COLUMN hasJob boolean;
+UPDATE roommates SET hasJob = true;
+UPDATE roommates SET hasJob = false WHERE id = 5;
+
+-- TODO try query that will fail
+-- ! SELECT * FROM roommates GROUP BY location
+-- SELECT location FROM roommates GROUP BY location -- same as distinct
+-- SELECT location, COUNT(id), FROM roommates GROUP BY location
+
+-- SOME more features
+ALTER TABLE roommates ADD COLUMN gender boolean, ADD COLUMN age integer;
+
+UPDATE roommates SET gender = false, age = 23;
+UPDATE roommates SET gender = true, age = 25 WHERE id=3;
+
+UPDATE roommates SET age = 24 WHERE id IN (1,2);
+
+-- SELECT origin, COUNT(id), AVG(age)
+-- FROM roommates
+-- GROUP BY origin;
+
+-- Multiple grouping example
+
+-- SELECT origin, gender, COUNT(id), AVG(age)
+-- FROM roommates
+-- GROUP BY origin, gender;
+
+-- Endangered species
+
+-- SELECT origin, gender, COUNT(id), AVG(age)
+-- FROM roommates
+-- GROUP BY origin, gender
+-- HAVING COUNT(id) < 3;
+
+-- TODO relevant part for this file is here
+CREATE INDEX name_index ON roommates USING btree(
+    name ASC NULLS LAST
+);
